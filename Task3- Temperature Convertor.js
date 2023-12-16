c3537e8615bbb957090f478c30a71d200f23dc4c
Shreeyaa14 <151328765+Shreeyaa14@users.noreply.github.com>
2023-12-16 19:07:46 +0530

    Update Task3- Temperature Convertor.js

--- Task3- Temperature Convertor.js
+++ Task3- Temperature Convertor.js
@@ -1,1 +1,23 @@
+function convertTemperature() {
+    // Get user input
+    var temperatureInput = document.getElementById('temperature').value;
+    var unitInput = document.getElementById('unit').value;
 
+    // Validate input is a number
+    if (isNaN(temperatureInput)) {
+        alert("Please enter a valid number for temperature.");
+        return;
+    }
+
+    // Convert temperature
+    var convertedTemperature;
+    if (unitInput === 'celsius') {
+        convertedTemperature = parseFloat(temperatureInput);
+    } else if (unitInput === 'fahrenheit') {
+        convertedTemperature = (parseFloat(temperatureInput) - 32) * 5/9;
+    } else {
+        convertedTemperature = parseFloat(temperatureInput) - 273.15;
+    }
+
+    document.getElementById('result').innerHTML = convertedTemperature.toFixed(2);
+}
